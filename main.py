7cfc899e664b76d7b89475a49d249e4f44274142
VANSHIKA SINGHAL <149135506+vanshikasinghal90@users.noreply.github.com>
2025-11-01 15:36:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+pip install tensorflow==2.13.0
+pip install opencv-python
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install pillow
+pip install scikit-learn
+pip install xmltodict  # for parsing annotations
